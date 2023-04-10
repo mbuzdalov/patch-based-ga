c820a4da8f3b8afb350eac23d11bc88b2e7a2c4c
Maxim Buzdalov <mbuzdalov@gmail.com>
2023-04-10 23:01:13 +0100

    Implement (1+1) EA.

---
+++ src/main/scala/com/github/mbuzdalov/patchga/algorithm/OnePlusOneEA.scala
@@ -0,0 +1,28 @@
+package com.github.mbuzdalov.patchga.algorithm
+
+import scala.annotation.tailrec
+
+import com.github.mbuzdalov.patchga.config.*
+import com.github.mbuzdalov.patchga.distribution.{BinomialDistribution, IntegerDistribution}
+
+class OnePlusOneEA(distributionSource: Int => IntegerDistribution) extends Optimizer:
+  type RequiredConfig = FitnessType & PatchSizeType & Population & IntegralPatchSize & MaximumPatchSize & FitnessComparator & RandomProvider
+
+  def optimize(conf: RequiredConfig): Nothing =
+    import conf._
+    val distribution = distributionSource(conf.patchSizeToInt(conf.maximumPatchSize))
+
+    @tailrec
+    def go(curr: IndividualHandle): Nothing =
+      val next = mutateH(curr, patchSizeFromInt(distribution.sample(random)))
+      if compare(fitnessH(curr), fitnessH(next)) <= 0 then
+        discardH(curr)
+        go(next)
+      else
+        discardH(next)
+        go(curr)
+
+    go(newRandomIndividualH())
+
+object OnePlusOneEA:
+  val withStandardBitMutation: OnePlusOneEA = OnePlusOneEA(n => BinomialDistribution(n, 1.0 / n))
--- src/test/scala/com/github/mbuzdalov/patchga/NaiveOneMaxTests.scala
+++ src/test/scala/com/github/mbuzdalov/patchga/NaiveOneMaxTests.scala
@@ -2,21 +2,20 @@ package com.github.mbuzdalov.patchga
 
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
-
-import com.github.mbuzdalov.patchga.algorithm.RandomizedLocalSearch
+import com.github.mbuzdalov.patchga.algorithm.{OnePlusOneEA, Optimizer, RandomizedLocalSearch}
 import com.github.mbuzdalov.patchga.config.{FitnessComparator, FitnessType, IndividualType, SimpleFitnessFunction}
 import com.github.mbuzdalov.patchga.infra.{FixedTargetTerminator, ThreadLocalRandomProvider}
 import com.github.mbuzdalov.patchga.population.NaiveScratchPopulation
 import com.github.mbuzdalov.patchga.representation.UnconstrainedBitString
 import com.github.mbuzdalov.patchga.problem.OneMax
 
-class RLSonOneMax extends AnyFlatSpec with Matchers:
+class NaiveOneMaxTests extends AnyFlatSpec with Matchers:
   private class NaiveOneMax(size: Int) extends UnconstrainedBitString(size), OneMax, NaiveScratchPopulation, ThreadLocalRandomProvider, FixedTargetTerminator:
     override def targetFitness: Fitness = size
 
   private case class RunResults(avgEvaluations: Double, avgTime: Double)
 
-  private def runRLS(problem: => RandomizedLocalSearch.RequiredConfig & FixedTargetTerminator): RunResults =
+  private def run(optimizer: Optimizer)(problem: => optimizer.RequiredConfig & FixedTargetTerminator): RunResults =
     val nRuns = 10
     var sumEvaluations = 0.0
     val tBegin = System.nanoTime()
@@ -24,15 +23,20 @@ class RLSonOneMax extends AnyFlatSpec with Matchers:
     while t < nRuns do
       val instance = problem
       try
-        RandomizedLocalSearch.optimize(instance)
+        optimizer.optimize(instance)
       catch
         case e: instance.TargetReached => sumEvaluations += e.nEvaluations
       t += 1
     RunResults(sumEvaluations / nRuns, (System.nanoTime() - tBegin) * 1e-9 / nRuns)
 
-  "RLS on OneMax" should "work well with naive population" in {
+  private def simpleTest(expected: Int => Double)(optimizer: Optimizer)(problem: Int => optimizer.RequiredConfig & FixedTargetTerminator): Unit =
     val n = 512
-    val expectedEvs = n * math.log(n)
-    val RunResults(evs, _) = runRLS(new NaiveOneMax(n))
+    val expectedEvs = expected(n)
+    val RunResults(evs, _) = run(optimizer)(problem(n))
     evs shouldBe expectedEvs +- (0.3 * expectedEvs)
-  }
+
+  "RLS on OneMax" should "work well with naive population" in
+    simpleTest(n => n * math.log(n))(RandomizedLocalSearch)(n => new NaiveOneMax(n))
+
+  "(1+1) EA on OneMax" should "work well with naive population" in
+    simpleTest(n => math.E * n * math.log(n))(OnePlusOneEA.withStandardBitMutation)(n => new NaiveOneMax(n))
