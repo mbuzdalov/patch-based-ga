83d0dc1ed4e5b7c45bf46c7b8dc686a743726eac
Maxim Buzdalov <mbuzdalov@gmail.com>
2023-04-15 10:58:32 +0100

    Return naive binomial distribution impl for large p.

--- src/main/scala/com/github/mbuzdalov/patchga/distribution/BinomialDistribution.scala
+++ src/main/scala/com/github/mbuzdalov/patchga/distribution/BinomialDistribution.scala
@@ -1,20 +1,32 @@
 package com.github.mbuzdalov.patchga.distribution
 import java.util.Random
 
-class BinomialDistribution private (n: Int, p: Double) extends IntegerDistribution:
-  private val log1p = math.log1p(-p)
-  private def next(from: Long, rng: Random): Long = (from + math.log(rng.nextDouble()) / log1p).toLong
-  override def min: Int = 0
-  override def max: Int = n
-  override def sample(rng: Random): Int =
-    var result = 0
-    var ptr = next(0, rng)
-    while ptr < n do
-      result += 1
-      ptr = next(ptr + 1, rng)
-    result  
+import com.github.mbuzdalov.patchga.util.Loops
 
 object BinomialDistribution:
+  private class LogBasedBinomialDistribution(n: Int, p: Double) extends IntegerDistribution:
+    private val log1p = math.log1p(-p)
+    private def next(from: Long, rng: Random): Long = (from + math.log(rng.nextDouble()) / log1p).toLong
+    override def min: Int = 0
+    override def max: Int = n
+    override def sample(rng: Random): Int =
+      var result = 0
+      var ptr = next(0, rng)
+      while ptr < n do
+        result += 1
+        ptr = next(ptr + 1, rng)
+      result
+
+  private class NaiveBinomialDistribution(n: Int, p: Double) extends IntegerDistribution:
+    override def min: Int = 0
+
+    override def max: Int = n
+
+    override def sample(rng: Random): Int =
+      var result = 0
+      Loops.loop(0, n)(_ => if rng.nextDouble() < p then result += 1)
+      result
+
   def apply(n: Int, p: Double): IntegerDistribution =
     if p < 0 || p > 1 then
       throw new IllegalArgumentException(s"p is out of bounds: $p is not in [0;1]")
@@ -24,6 +36,8 @@ object BinomialDistribution:
       ConstantDistribution.zero
     else if p == 1 then
       ConstantDistribution(n)
+    else if p < 0.05 then
+      new LogBasedBinomialDistribution(n, p)
     else
-      new BinomialDistribution(n, p)
+      new NaiveBinomialDistribution(n, p)
       
