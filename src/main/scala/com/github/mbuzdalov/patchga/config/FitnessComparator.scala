72e8ed0c5cef4a5e28e1c895cc48d223afb76fb8
Maxim Buzdalov <mbuzdalov@gmail.com>
2023-04-25 11:10:31 +0100

    Introduce a given ordering for fitness along with FitnessComparator.

--- src/main/scala/com/github/mbuzdalov/patchga/config/FitnessComparator.scala
+++ src/main/scala/com/github/mbuzdalov/patchga/config/FitnessComparator.scala
@@ -3,4 +3,5 @@ package com.github.mbuzdalov.patchga.config
 trait FitnessComparator:
   self: FitnessType =>
     def compare(lhs: Fitness, rhs: Fitness): Int
+    given Ordering[Fitness] = (x: Fitness, y: Fitness) => compare(x, y)
     
