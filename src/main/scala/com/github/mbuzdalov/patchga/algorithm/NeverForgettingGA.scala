a26a50985efbe524410266a39347de86c92d12ac
Maxim Buzdalov <mbuzdalov@gmail.com>
2026-01-10 08:07:14 +0000

    Refactor NFGA using Loops.forever and avoiding a special type for sorted vector (which allowed factoring out parent sampling).

--- src/main/scala/com/github/mbuzdalov/patchga/algorithm/NeverForgettingGA.scala
+++ src/main/scala/com/github/mbuzdalov/patchga/algorithm/NeverForgettingGA.scala
@@ -2,67 +2,69 @@ package com.github.mbuzdalov.patchga.algorithm
 
 import com.github.mbuzdalov.patchga.config.*
 import com.github.mbuzdalov.patchga.distribution.PowerLawDistribution
-import com.github.mbuzdalov.patchga.util.SortedVector
+import com.github.mbuzdalov.patchga.util.Loops
 
 import scala.annotation.tailrec
 import scala.collection.mutable.ArrayBuffer
 
-class NeverForgettingGA(mutationParentSelectionBeta: Double,
+class NeverForgettingGA(firstParentSelectionBeta: Double,
                         mutationDistanceBeta: Double,
                         crossoverProbability: Double,
-                        crossoverParentDistanceSelectionBeta: Double,
-                        crossoverParentPairSelectionBeta: Double,
+                        crossoverParentMinimumDistanceBeta: Double,
+                        secondParentSelectionBeta: Double,
                         crossoverDistanceBeta: Double) extends Optimizer:
   type RequiredConfig = FitnessType & Population & MaximumPatchSize & FitnessComparator & RandomProvider
-
+  
   override def optimize(config: RequiredConfig): Nothing =
     import config.*
-
+    
     val crossoverSecondParentBuffer = new ArrayBuffer[IndividualHandle]()
     val distanceBuffer = new ArrayBuffer[Int]()
     val distanceSeen = new Array[Boolean](maximumPatchSize + 1)
     val inverseFitnessOrdering = Ordering.by(fitnessH).reverse
-    val nodesSorted = new SortedVector[IndividualHandle](using inverseFitnessOrdering)
-    nodesSorted.add(newRandomIndividualH())
+    val nodesSorted = new ArrayBuffer[IndividualHandle]()
+    nodesSorted.addOne(newRandomIndividualH())
 
-    def sampleFirstParent(): IndividualHandle =
-      val index0 = PowerLawDistribution.sample(nodesSorted.size, mutationParentSelectionBeta, random) - 1
-      val fitness0 = fitnessH(nodesSorted(index0))
-      var indexLo = index0
-      while indexLo > 0 && compare(fitnessH(nodesSorted(indexLo - 1)), fitness0) == 0 do indexLo -= 1
-      var indexHi = index0
-      while indexHi + 1 < nodesSorted.size && compare(fitnessH(nodesSorted(indexHi + 1)), fitness0) == 0 do indexHi += 1
-      val index = random.nextInt(indexLo, indexHi + 1)
-      nodesSorted(index)
+    def insertionSortAdd(h: IndividualHandle): Unit =
+      nodesSorted.addOne(h)
+      var idx = nodesSorted.size - 2
+      while idx >= 0 && inverseFitnessOrdering.gt(nodesSorted(idx), h) do
+        nodesSorted(idx + 1) = nodesSorted(idx)
+        nodesSorted(idx) = h
+        idx -= 1
     
-    def sampleSecondParent(): IndividualHandle =
-      val index0 = PowerLawDistribution.sample(crossoverSecondParentBuffer.size, crossoverParentPairSelectionBeta, random) - 1
-      val fitness0 = fitnessH(crossoverSecondParentBuffer(index0))
+    def sampleParent(source: ArrayBuffer[IndividualHandle], beta: Double): IndividualHandle =
+      val index0 = PowerLawDistribution.sample(source.size, beta, random) - 1
+      val fitness0 = fitnessH(source(index0))
       var indexLo = index0
-      while indexLo > 0 && compare(fitnessH(crossoverSecondParentBuffer(indexLo - 1)), fitness0) == 0 do indexLo -= 1
+      while indexLo > 0 && compare(fitnessH(source(indexLo - 1)), fitness0) == 0 do indexLo -= 1
       var indexHi = index0
-      while indexHi + 1 < crossoverSecondParentBuffer.size && compare(fitnessH(crossoverSecondParentBuffer(indexHi + 1)), fitness0) == 0 do indexHi += 1
+      while indexHi + 1 < source.size && compare(fitnessH(source(indexHi + 1)), fitness0) == 0 do indexHi += 1
       val index = random.nextInt(indexLo, indexHi + 1)
-      crossoverSecondParentBuffer(index)
+      source(index)
+    
+    def sampleFirstParent(): IndividualHandle = sampleParent(nodesSorted, firstParentSelectionBeta)
+    def sampleSecondParent(): IndividualHandle = sampleParent(crossoverSecondParentBuffer, secondParentSelectionBeta)
     
     @tailrec
-    def go(): Nothing =
+    def sampleFirstParentWithDistantEnoughNeighbors(): IndividualHandle =
+      val parent = sampleFirstParent()
+      distanceBuffer.clear()
+      collectDistanceToHandles(parent, (_, d) => if d > 1 && !distanceSeen(d) then
+        distanceSeen(d) = true
+        distanceBuffer.addOne(d))
+      distanceBuffer.foreach(v => distanceSeen(v) = false)
+      if distanceBuffer.isEmpty then sampleFirstParentWithDistantEnoughNeighbors() else parent
+    
+    Loops.forever:
       // with three different nodes, at least two of them have distance > 1.
       val nextNode = if nodesSorted.size >= 3 && random.nextDouble() < crossoverProbability then
         // crossover
         // sample a parent such that it has individuals at distance > 1
-        var firstParent: IndividualHandle = sampleFirstParent()
-        while
-          distanceBuffer.clear()
-          collectDistanceToHandles(firstParent, (_, d) => if d > 1 && !distanceSeen(d) then
-            distanceSeen(d) = true
-            distanceBuffer.addOne(d))
-          distanceBuffer.foreach(v => distanceSeen(v) = false)
-          distanceBuffer.isEmpty
-        do firstParent = sampleFirstParent()
+        val firstParent = sampleFirstParentWithDistantEnoughNeighbors()
         // sample a distance out of the valid ones
         distanceBuffer.sortInPlace()
-        val secondParentDistance = distanceBuffer(PowerLawDistribution.sample(distanceBuffer.size, crossoverParentDistanceSelectionBeta, random) - 1)
+        val secondParentDistance = distanceBuffer(PowerLawDistribution.sample(distanceBuffer.size, crossoverParentMinimumDistanceBeta, random) - 1)
         // collect individuals at distance which is at least as much as the found distance, and sample one
         collectHandlesAtDistance(firstParent, _ >= secondParentDistance, crossoverSecondParentBuffer)
         crossoverSecondParentBuffer.sortInPlace()(using inverseFitnessOrdering)
@@ -78,6 +80,4 @@ class NeverForgettingGA(mutationParentSelectionBeta: Double,
         mutateH(sampleFirstParent(), change)
       end nextNode
       
-      if nextNode.referenceCount == 1 then nodesSorted.add(nextNode)
-      go()
-    go()
+      if nextNode.referenceCount == 1 then insertionSortAdd(nextNode)
