ca57276dd5a948f4f66593061a855eac086cd3e8
Maxim Buzdalov <mbuzdalov@gmail.com>
2026-01-12 03:22:53 +0000

    Fool-proof power-law distribution against non-positive bounds.

--- src/main/scala/com/github/mbuzdalov/patchga/distribution/PowerLawDistribution.scala
+++ src/main/scala/com/github/mbuzdalov/patchga/distribution/PowerLawDistribution.scala
@@ -6,14 +6,15 @@ import java.util.concurrent.ConcurrentHashMap
 import com.github.mbuzdalov.patchga.util.GrowableDoubleArray
 
 class PowerLawDistribution private(n: Int, val beta: Double, prefixSums: GrowableDoubleArray) extends IntegerDistribution:
+  assert(n > 1)
   override def min: Int = 1
   override def max: Int = n
-  override def sample(rng: Random): Int = PowerLawDistribution.sample(n, beta, rng, prefixSums)
+  override def sample(rng: Random): Int = PowerLawDistribution.sampleChecked(n, beta, rng, prefixSums)
 
 object PowerLawDistribution:
   private val distributionInstances = new ConcurrentHashMap[Double, GrowableDoubleArray]()
 
-  private def sample(n: Int, beta: Double, rng: Random, array: GrowableDoubleArray): Int = if n == 1 then 1 else
+  private def sampleChecked(n: Int, beta: Double, rng: Random, array: GrowableDoubleArray): Int =
     // array(x) contains the prefix sum for (1, 2, ..., x + 2)^{-beta}
     val maxIdx = n - 2
     if array.length <= maxIdx then
@@ -27,8 +28,16 @@ object PowerLawDistribution:
       var index = 0
       while query >= array(index) do index += 1
       index + 2
+  
+  private def sample(n: Int, beta: Double, rng: Random, array: GrowableDoubleArray): Int = 
+    if n <= 0 then throw IllegalArgumentException("Non-positive bound for power-law distribution")
+    else if n == 1 then 1
+    else sampleChecked(n, beta, rng, array)
 
   private def getArrayForBeta(beta: Double) = distributionInstances.computeIfAbsent(beta, _ => new GrowableDoubleArray)
 
   def sample(n: Int, beta: Double, rng: Random): Int = sample(n, beta, rng, getArrayForBeta(beta))
-  def apply(n: Int, beta: Double): IntegerDistribution = new PowerLawDistribution(n, beta, getArrayForBeta(beta))
+  def apply(n: Int, beta: Double): IntegerDistribution =
+    if n <= 0 then throw IllegalArgumentException("Non-positive bound for power-law distribution")
+    else if n == 1 then ConstantDistribution(1)
+    else new PowerLawDistribution(n, beta, getArrayForBeta(beta))
