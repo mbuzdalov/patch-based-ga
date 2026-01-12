b612cf44044a314511203eccbe53d26528f12dc3
Maxim Buzdalov <mbuzdalov@gmail.com>
2026-01-12 03:22:22 +0000

    Add addition/subtraction with a constant to integer distribution.

--- src/main/scala/com/github/mbuzdalov/patchga/distribution/IntegerDistribution.scala
+++ src/main/scala/com/github/mbuzdalov/patchga/distribution/IntegerDistribution.scala
@@ -7,3 +7,15 @@ trait IntegerDistribution:
   def max: Int
   def sample(rng: Random): Int
   
+  def + (that: Int): IntegerDistribution = IntegerDistribution.addConstant(this, +that)
+  def - (that: Int): IntegerDistribution = IntegerDistribution.addConstant(this, -that)
+      
+object IntegerDistribution:
+  private def addConstant(source: IntegerDistribution, constant: Int): IntegerDistribution =
+    val newMin = source.min + constant
+    val newMax = source.max + constant
+    assert(newMin <= newMax, "Overflow when adding a constant to a distribution")
+    new IntegerDistribution:
+      override def min: Int = newMin
+      override def max: Int = newMax
+      override def sample(rng: Random): Int = source.sample(rng) + constant
