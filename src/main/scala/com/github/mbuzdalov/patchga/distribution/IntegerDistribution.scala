48783b39a400beca69d825601a5df4dcf303ec1c
Maxim Buzdalov <mbuzdalov@gmail.com>
2026-04-01 02:26:54 +0100

    Add support for more integer distribution arithmetics.

--- src/main/scala/com/github/mbuzdalov/patchga/distribution/IntegerDistribution.scala
+++ src/main/scala/com/github/mbuzdalov/patchga/distribution/IntegerDistribution.scala
@@ -1,17 +1,43 @@
 package com.github.mbuzdalov.patchga.distribution
 
 import java.util.Random
+import scala.annotation.{publicInBinary, targetName}
 
 trait IntegerDistribution:
   def min: Int
   def max: Int
   def sample(rng: Random): Int
+
+  @targetName("add")
+  infix def + (that: Int): IntegerDistribution = IntegerDistribution.addConstant(this, +that)
   
-  def + (that: Int): IntegerDistribution = IntegerDistribution.addConstant(this, +that)
-  def - (that: Int): IntegerDistribution = IntegerDistribution.addConstant(this, -that)
+  @targetName("subtract")
+  infix def - (that: Int): IntegerDistribution = IntegerDistribution.addConstant(this, -that)
+
+  @targetName("multiply")
+  infix def * (that: Int): IntegerDistribution = IntegerDistribution.multiplyByConstant(this, that)
+
+  @targetName("negate")
+  def unary_- : IntegerDistribution = IntegerDistribution.multiplyByConstant(this, -1)
+  
   def symmetric(reflection: Int): IntegerDistribution = IntegerDistribution.symmetric(this, reflection)
+end IntegerDistribution
 
 object IntegerDistribution:
+  extension (constant: Int)
+    def + (that: IntegerDistribution): IntegerDistribution = addConstant(that, constant)
+    def - (that: IntegerDistribution): IntegerDistribution = addConstant(-that, constant)
+    def * (that: IntegerDistribution): IntegerDistribution = multiplyByConstant(that, constant)
+  
+  private def multiplyByConstant(source: IntegerDistribution, constant: Int): IntegerDistribution =
+    val newMin = math.min(source.min * constant, source.max * constant)
+    val newMax = math.max(source.min * constant, source.max * constant)
+    assert(newMin <= newMax, "Overflow when adding a constant to a distribution")
+    new IntegerDistribution:
+      override def min: Int = newMin
+      override def max: Int = newMax
+      override def sample(rng: Random): Int = source.sample(rng) * constant
+  
   private def addConstant(source: IntegerDistribution, constant: Int): IntegerDistribution =
     val newMin = source.min + constant
     val newMax = source.max + constant
