dbe014305143ef4b858a6f833c4c8a0147d81137
Maxim Buzdalov <mbuzdalov@gmail.com>
2026-04-15 09:28:35 +0100

    Simplified plotting output for a somewhat different paper pipeline.

--- src/main/scala/com/github/mbuzdalov/patchga/util/CollectorPlotter.scala
+++ src/main/scala/com/github/mbuzdalov/patchga/util/CollectorPlotter.scala
@@ -5,8 +5,7 @@ import java.nio.file.{Files, Path, Paths}
 import scala.util.Using
 
 object CollectorPlotter:
-  private case class Algorithm(name: String, style: String, priority: Int)
-  private case class ResultHeader(problem: String, size: Int, algorithm: Algorithm)
+  private case class ResultHeader(problem: String, size: Int, algorithm: String)
   
   private def parseProblemName(name: String): (String, Int) = name match
     case s"linear-$size-$suf" => (s"linear-$suf", size.toInt)
@@ -20,16 +19,29 @@ object CollectorPlotter:
     case s"plateau$suf" => size
     case s"cliff$suf" => size
   
-  private def collect(path: Path, rewriteMap: Map[String, Option[Algorithm]]): Map[ResultHeader, IndexedSeq[Int]] =
-    val builder = IndexedSeq.newBuilder[(ResultHeader, Int)]
+  private class AlgoRun(path: Path):
+    def queries(): IndexedSeq[Int] = Using.resource(Files.newBufferedReader(path)): br =>
+      val builder = IndexedSeq.newBuilder[Int]
+      var line: String = null
+      while
+        line = br.readLine()
+        line != null
+      do
+        builder += line.toInt
+      builder.result()
+  
+  private def collect(path: Path): Map[ResultHeader, IndexedSeq[AlgoRun]] =
+    val builder = IndexedSeq.newBuilder[(ResultHeader, AlgoRun)]
     Files.list(path).filter(_.getFileName.toString.endsWith(".log")).forEach: logFile =>
       Files.lines(logFile).forEach:
         case s"$algo on $problem: run $runNo finished, $ordNo out of $totalNo, in $time ms, result $evaluations" =>
-          rewriteMap(algo) match
-            case None =>
-            case Some(algoName) =>
-              val (pName, pSize) = parseProblemName(problem)
-                builder.addOne(ResultHeader(pName, pSize, algoName) -> evaluations.toInt)
+          val detailFileRoot = logFile.resolveSibling(logFile.getFileName.toString.replace(".log", "")).resolve(problem).resolve(algo)
+          val df1 = detailFileRoot.resolve(s"${runNo.toInt}.txt")
+          val df2 = detailFileRoot.resolve(f"${runNo.toInt}%02d.txt")
+          val detailFile = if Files.exists(df1) then df1 else df2
+          val run = AlgoRun(detailFile)
+          val (pName, pSize) = parseProblemName(problem)
+          builder.addOne(ResultHeader(pName, pSize, algo) -> run)
         case other => if other.nonEmpty then println(s"$logFile: string $other not parsed")
     builder.result().groupBy(_._1).map((k, v) => (k, v.map(_._2)))
   
@@ -39,47 +51,47 @@ object CollectorPlotter:
     then line.substring(prefix.length)
     else throw IllegalArgumentException(s"Expected option '$what'")
   
-  private def parseRewrites(br: BufferedReader): Map[String, Option[Algorithm]] =
-    val builder = Map.newBuilder[String, Option[Algorithm]]
-    var line: String = null
-    while
-      line = br.readLine()
-      line != null
-    do
-      val key = line.trim
-      val name = br.readLine()
-      if name == "- ignore" then
-        builder.addOne(name -> None)
-      else
-        val realName = expect(name, "- name")
-        val style = expect(br.readLine(), "- style")
-        val priority = expect(br.readLine(), "- priority").toInt
-        builder.addOne(key -> Some(Algorithm(realName, style, priority)))
-      end if
-    end while
-    builder.result()
+  private def maxOut(seq: IndexedSeq[Int]): IndexedSeq[Int] =
+    seq.foldLeft(IndexedSeq.empty[Int]): (seq, v) =>
+      if seq.isEmpty
+      then seq.appended(v)
+      else seq.appended(math.max(v, seq.last))
+  
+  private def readSizes(br: BufferedReader): IArray[Int] =
+    IArray.unsafeFromArray(expect(br.readLine(), "anytime-sizes").split(" ").map(_.toInt))
   
   def main(args: Array[String]): Unit =
     val cfgFile = Paths.get(args(0))
     Using.resource(Files.newBufferedReader(Paths.get(args(0)))): cfg =>
       val src = cfgFile.resolveSibling(expect(cfg.readLine(), "source-dir"))
       val resultRoot = cfgFile.resolveSibling(expect(cfg.readLine(), "dest-dir"))
-      val rewrites = parseRewrites(cfg)
+      val anytimeSizes = readSizes(cfg)
       Files.createDirectories(resultRoot)
-      val results = collect(src, rewrites)
-      val allProblems = results.keys.map(_.problem).toIndexedSeq.distinct.sorted
-      for problem <- allProblems do
-        Using.resource(Files.newBufferedWriter(resultRoot.resolve(s"$problem.tex"))): out =>
-          val allAlgorithms = results.keys.filter(_.problem == problem).map(_.algorithm).toIndexedSeq.distinct.sortBy(_.priority)
-          for algorithm <- allAlgorithms do
-            out.write(s"\\addplot[${algorithm.style}, error bars/.cd, y dir=both, y explicit] coordinates{")
-            for size <- results.keys.filter(k => k.problem == problem && k.algorithm == algorithm).map(_.size).toIndexedSeq.distinct.sorted do
+      val results = collect(src)
+      for (problem, withProblem) <- results.groupBy(_._1.problem) do
+        for (algorithm, withAlgorithm) <- withProblem.groupBy(_._1.algorithm) do
+          Using.resource(Files.newBufferedWriter(resultRoot.resolve(s"ev-$problem-$algorithm.csv"))): out =>
+            out.write("size,avg,std,avg-norm,std-norm,q1,q2,q3,q1-norm,q2-norm,q3-norm\n")
+            for (size, withSize) <- withAlgorithm.groupBy(_._1.size).toIndexedSeq.sortBy(_._1).map(p => { assert(p._2.size == 1); (p._1, p._2.head._2) }) do
               val myDivisor = divisor(problem, size)
-              for h <- results.keys.filter(k => k.problem == problem && k.algorithm == algorithm && k.size == size) do
-                val evaluations = results(h).sorted.map(_ / myDivisor)
-                //assert(evaluations.size == 51)
-                val avg = evaluations.sum / evaluations.size
-                val std = math.sqrt(evaluations.map(e => math.pow(e - avg, 2)).sum / (evaluations.size - 1))
-                out.write(s"($size,$avg)+-(0,$std)")
-            out.write(s"};\n\\addlegendentry{${algorithm.name}}\n")
-            
+              val evaluations = withSize.map(r => r.queries().size.toDouble).sorted
+              //assert(evaluations.size == 51)
+              val avg = evaluations.sum / evaluations.size
+              val std = math.sqrt(evaluations.map(e => math.pow(e - avg, 2)).sum / (evaluations.size - 1))
+              val median = evaluations(evaluations.size / 2)
+              val q1 = evaluations(evaluations.size / 4)
+              val q3 = evaluations(evaluations.size - 1 - evaluations.size / 4)
+              out.write(s"$size,$avg,$std,${avg / myDivisor},${std / myDivisor},$q1,$median,$q3,${q1 / myDivisor},${median / myDivisor},${q3 / myDivisor}\n")
+        for mySize <- anytimeSizes do
+          for (algorithm, withAlgorithm) <- withProblem.filter(_._1.size == mySize).groupBy(_._1.algorithm).map(p => { assert(p._2.size == 1); (p._1, p._2.head._2)}) do
+            Using.resource(Files.newBufferedWriter(resultRoot.resolve(s"rt-$problem-$mySize-$algorithm.csv"))): out =>
+              out.write("query,q1,q2,q3,q1-inv,q2-inv,q3-inv\n")
+              val allRunsRaw = withAlgorithm.map(r => maxOut(r.queries()))
+              val maxSize = allRunsRaw.map(_.size).max
+              val quads = IndexedSeq.tabulate(maxSize): i =>
+                val slice = allRunsRaw.map(seq => if seq.size > i then seq(i) else seq.last).sorted
+                (slice(slice.size / 4), slice(slice.size / 2), slice(slice.size - 1 - slice.size / 4))
+              Loops.foreach(0, maxSize): i =>
+                if i == 0 || i == maxSize - 1 || quads(i) != quads(i - 1) || quads(i) != quads(i + 1) then
+                  val (q1, q2, q3) = quads(i)
+                  out.write(s"${i + 1},$q1,$q2,$q3,${mySize - q1 + 1},${mySize - q2 + 1},${mySize - q3 + 1}\n")
