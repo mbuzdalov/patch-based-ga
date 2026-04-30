1187735b4107626a996f4df7ee7b8c5a3c778367
Maxim Buzdalov <mbuzdalov@gmail.com>
2026-04-30 15:52:13 +0100

    Add more crossovers to the knapsack test suite.

--- src/main/scala/com/github/mbuzdalov/patchga/main/KnapsackQualityMeasurements.scala
+++ src/main/scala/com/github/mbuzdalov/patchga/main/KnapsackQualityMeasurements.scala
@@ -1,7 +1,7 @@
 package com.github.mbuzdalov.patchga.main
 
 import com.github.mbuzdalov.patchga.algorithm.*
-import com.github.mbuzdalov.patchga.distribution.PowerLawDistribution
+import com.github.mbuzdalov.patchga.distribution.{BinomialDistribution, ConstantDistribution, PowerLawDistribution, UniformDistribution}
 import com.github.mbuzdalov.patchga.infra.CheapFixedBudgetTerminator
 import com.github.mbuzdalov.patchga.problem.Problems
 import com.github.mbuzdalov.patchga.util.MeanAndStandardDeviation
@@ -25,6 +25,11 @@ object KnapsackQualityMeasurements:
         "(10+1) hGA" -> MuPlusOneGA.withPowerLawMutation(10, 1, 1.5),
         "NFGA [dist 1.5 other 2.5]" -> new NeverForgettingGA(2.5, 1.5, 2.5, 0.5, 1.5, None, 2.5, d => PowerLawDistribution(d - 1, 2.5).symmetric(d)),
         "NFGA [all 1.5]" -> new NeverForgettingGA(1.5, 1.5, 1.5, 0.5, 1.5, None, 1.5, d => PowerLawDistribution(d - 1, 1.5).symmetric(d)),
+        "NFGA [all 1.5, UX]" -> new NeverForgettingGA(1.5, 1.5, 1.5, 0.5, 1.5, None, 1.5, d => BinomialDistribution(d - 2, 0.5) + 1),
+        "NFGA [all 1.5, US]" -> new NeverForgettingGA(1.5, 1.5, 1.5, 0.5, 1.5, None, 1.5, d => UniformDistribution(1, d - 1)),
+        "NFGA [all 1.5, F1]" -> new NeverForgettingGA(1.5, 1.5, 1.5, 0.5, 1.5, None, 1.5, d => ConstantDistribution(1)),
+        "NFGA [all 1.5, S1]" -> new NeverForgettingGA(1.5, 1.5, 1.5, 0.5, 1.5, None, 1.5, d => ConstantDistribution(d - 1)),
+        "DEGA+" -> DEGAPlus,
       )
       case "(2+1)" => for cc <- 8 to 15; c = (1 << cc) * 0.001 yield
         s"(2+1) EA [$c]" -> MuPlusOneGA.withStandardBitMutation(2, 1, c)
