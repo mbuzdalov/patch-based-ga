f13ef42a676ced9113b8ad868cb03fb5c3c064d8
Maxim Buzdalov <mbuzdalov@gmail.com>
2026-05-21 17:19:30 +0100

    Add DEGA to the OneMax suite, support multiple test sizes.

--- src/test/scala/com/github/mbuzdalov/patchga/IncrementalOneMaxTests.scala
+++ src/test/scala/com/github/mbuzdalov/patchga/IncrementalOneMaxTests.scala
@@ -1,6 +1,6 @@
 package com.github.mbuzdalov.patchga
 
-import com.github.mbuzdalov.patchga.algorithm.{MuPlusOneGA, OnePlusOneEA, Optimizer}
+import com.github.mbuzdalov.patchga.algorithm.{DEGAPlus, MuPlusOneGA, OnePlusOneEA, Optimizer}
 import com.github.mbuzdalov.patchga.infra.FixedTargetTerminator
 import com.github.mbuzdalov.patchga.problem.Problems
 import com.github.mbuzdalov.patchga.util.Loops
@@ -20,33 +20,38 @@ class IncrementalOneMaxTests extends AnyFlatSpec with Matchers:
       sumEvaluations += FixedTargetTerminator.runUntilTargetReached(optimizer, instance, target).nEvaluations
     RunResults(sumEvaluations / nRuns, (System.nanoTime() - tBegin) * 1e-9 / nRuns)
 
-  private def simpleTest(expected: Int => Double)
+  private def simpleTest(ns: Int*)
+                        (expected: Int => Double)
                         (optimizer: Optimizer)
                         (problem: Int => optimizer.RequiredConfig & Problems.IntProblem): Unit =
-    val n = 1024
-    val expectedEvs = expected(n)
-    val RunResults(evs, _) = run(optimizer, n)(problem(n))
-    evs shouldBe expectedEvs +- (0.22 * expectedEvs)
+    for n <- ns do
+      val expectedEvs = expected(n)
+      val RunResults(evs, _) = run(optimizer, n)(problem(n))
+      evs shouldBe expectedEvs +- (0.3 * expectedEvs)
 
   "RLS on OneMax" should "work well with single-slot MST-based population" in
-    simpleTest(n => n * math.log(n))
+    simpleTest(256, 512, 1024)
+              (n => n * math.log(n))
               (OnePlusOneEA.randomizedLocalSearch)
               (n => Problems.incrementalOneMaxFT(n, allowDuplicates = true, disableDiscard = false))
 
   "(1+1) EA on OneMax" should "work well with single-slot MST-based population" in
-    simpleTest(n => math.E * n * math.log(n))
+    simpleTest(256, 512, 1024)
+              (n => math.E * n * math.log(n))
               (OnePlusOneEA.withStandardBitMutation)
               (n => Problems.incrementalOneMaxFT(n, allowDuplicates = true, disableDiscard = false))
 
   // Constants for (2+1) GA are taken from https://link.springer.com/article/10.1007/s00453-021-00893-w.
 
   "(2+1) GA on OneMax" should "work well with single-slot MST-based population using c=1" in
-    simpleTest(n => 2.224 * n * math.log(n))
+    simpleTest(256, 512, 1024)
+              (n => 2.224 * n * math.log(n))
               (MuPlusOneGA.withStandardBitMutation(2, 1.0, 1))
               (n => Problems.incrementalOneMaxFT(n, allowDuplicates = true, disableDiscard = false))
 
   it should "work well with single-slot MST-based population using c=1.2122" in
-    simpleTest(n => 2.18417 * n * math.log(n))
+    simpleTest(256, 512, 1024)
+              (n => 2.18417 * n * math.log(n))
               (MuPlusOneGA.withStandardBitMutation(2, 1.0, 1.2122))
               (n => Problems.incrementalOneMaxFT(n, allowDuplicates = true, disableDiscard = false))
 
@@ -54,6 +59,13 @@ class IncrementalOneMaxTests extends AnyFlatSpec with Matchers:
   // but they underestimate the runtime for the used problem sizes.
 
   "(10+1) GA on OneMax" should "work well with single-slot MST-based population" in
-    simpleTest(n => 1.75 * n * math.log(n))
+    simpleTest(256, 512, 1024)
+              (n => 1.75 * n * math.log(n))
               (MuPlusOneGA.withStandardBitMutation(10, 1.0, 1.43))
+              (n => Problems.incrementalOneMaxFT(n, allowDuplicates = true, disableDiscard = false))
+  
+  "DEGA+ on OneMax" should "work well with single-slot MST-based population" in
+    simpleTest(64, 96, 128, 150)
+              (n => 2.1 * n * math.log(n))
+              (DEGAPlus.withStandardBitMutation)
               (n => Problems.incrementalOneMaxFT(n, allowDuplicates = true, disableDiscard = false))
--- src/test/scala/com/github/mbuzdalov/patchga/NaiveOneMaxTests.scala
+++ src/test/scala/com/github/mbuzdalov/patchga/NaiveOneMaxTests.scala
@@ -1,6 +1,6 @@
 package com.github.mbuzdalov.patchga
 
-import com.github.mbuzdalov.patchga.algorithm.{MuPlusOneGA, OnePlusOneEA, Optimizer}
+import com.github.mbuzdalov.patchga.algorithm.{DEGAPlus, MuPlusOneGA, OnePlusOneEA, Optimizer}
 import com.github.mbuzdalov.patchga.infra.FixedTargetTerminator
 import com.github.mbuzdalov.patchga.problem.Problems
 import com.github.mbuzdalov.patchga.util.Loops
@@ -20,53 +20,62 @@ class NaiveOneMaxTests extends AnyFlatSpec with Matchers:
       sumEvaluations += FixedTargetTerminator.runUntilTargetReached(optimizer, instance, target).nEvaluations
     RunResults(sumEvaluations / nRuns, (System.nanoTime() - tBegin) * 1e-9 / nRuns)
 
-  private def simpleTest(expected: Int => Double)
+  private def simpleTest(ns: Int*)
+                        (expected: Int => Double)
                         (optimizer: Optimizer)
                         (problem: Int => optimizer.RequiredConfig & Problems.IntProblem): Unit =
-    val n = 512
-    val expectedEvs = expected(n)
-    val RunResults(evs, _) = run(optimizer, n)(problem(n))
-    evs shouldBe expectedEvs +- (0.3 * expectedEvs)
+    for n <- ns do
+      val expectedEvs = expected(n)
+      val RunResults(evs, _) = run(optimizer, n)(problem(n))
+      evs shouldBe expectedEvs +- (0.3 * expectedEvs)
 
   "RLS on OneMax" should "work well with naive population w/o genealogy" in
-    simpleTest(n => n * math.log(n))
+    simpleTest(256, 512, 1024)
+              (n => n * math.log(n))
               (OnePlusOneEA.randomizedLocalSearch)
               (n => Problems.naiveOneMaxFT(n, allowDuplicates = true, disableDiscard = false, supportGenealogy = false))
   
   it should "work well with naive population with genealogy" in
-    simpleTest(n => n * math.log(n))
+    simpleTest(256, 512, 1024)
+              (n => n * math.log(n))
               (OnePlusOneEA.randomizedLocalSearch)
               (n => Problems.naiveOneMaxFT(n, allowDuplicates = true, disableDiscard = false, supportGenealogy = true))
   
   "(1+1) EA on OneMax" should "work well with naive population w/o genealogy" in
-    simpleTest(n => math.E * n * math.log(n))
+    simpleTest(256, 512, 1024)
+              (n => math.E * n * math.log(n))
               (OnePlusOneEA.withStandardBitMutation)
               (n => Problems.naiveOneMaxFT(n, allowDuplicates = true, disableDiscard = false, supportGenealogy = false))
   
   it should "work well with naive population with genealogy" in
-    simpleTest(n => math.E * n * math.log(n))
+    simpleTest(256, 512, 1024)
+              (n => math.E * n * math.log(n))
               (OnePlusOneEA.withStandardBitMutation)
               (n => Problems.naiveOneMaxFT(n, allowDuplicates = true, disableDiscard = false, supportGenealogy = true))
   
   // Constants for (2+1) GA are taken from https://link.springer.com/article/10.1007/s00453-021-00893-w.
   
   "(2+1) GA on OneMax" should "work well with naive population using c=1 w/o genealogy" in
-    simpleTest(n => 2.224 * n * math.log(n))
+    simpleTest(256, 512, 1024)
+              (n => 2.224 * n * math.log(n))
               (MuPlusOneGA.withStandardBitMutation(2, 1.0, 1))
               (n => Problems.naiveOneMaxFT(n, allowDuplicates = true, disableDiscard = false, supportGenealogy = false))
   
   it should "work well with naive population using c=1 with genealogy" in
-    simpleTest(n => 2.224 * n * math.log(n))
+    simpleTest(256, 512, 1024)
+              (n => 2.224 * n * math.log(n))
               (MuPlusOneGA.withStandardBitMutation(2, 1.0, 1))
               (n => Problems.naiveOneMaxFT(n, allowDuplicates = true, disableDiscard = false, supportGenealogy = true))
   
   it should "work well with naive population using c=1.2122 w/o genealogy" in
-    simpleTest(n => 2.18417 * n * math.log(n))
+    simpleTest(256, 512, 1024)
+              (n => 2.18417 * n * math.log(n))
               (MuPlusOneGA.withStandardBitMutation(2, 1.0, 1.2122))
               (n => Problems.naiveOneMaxFT(n, allowDuplicates = true, disableDiscard = false, supportGenealogy = false))
   
   it should "work well with naive population using c=1.2122 with genealogy" in
-    simpleTest(n => 2.18417 * n * math.log(n))
+    simpleTest(256, 512, 1024)
+              (n => 2.18417 * n * math.log(n))
               (MuPlusOneGA.withStandardBitMutation(2, 1.0, 1.2122))
               (n => Problems.naiveOneMaxFT(n, allowDuplicates = true, disableDiscard = false, supportGenealogy = true))
   
@@ -74,11 +83,26 @@ class NaiveOneMaxTests extends AnyFlatSpec with Matchers:
   // but they underestimate the runtime for the used problem sizes.
   
   "(10+1) GA on OneMax" should "work well with naive population w/o genealogy" in
-    simpleTest(n => 1.75 * n * math.log(n))
+    simpleTest(256, 512, 1024)
+              (n => 1.75 * n * math.log(n))
               (MuPlusOneGA.withStandardBitMutation(10, 1.0, 1.43))
               (n => Problems.naiveOneMaxFT(n, allowDuplicates = true, disableDiscard = false, supportGenealogy = false))
   
   it should "work well with naive population with genealogy" in
-    simpleTest(n => 1.75 * n * math.log(n))
+    simpleTest(256, 512, 1024)
+              (n => 1.75 * n * math.log(n))
               (MuPlusOneGA.withStandardBitMutation(10, 1.0, 1.43))
+              (n => Problems.naiveOneMaxFT(n, allowDuplicates = true, disableDiscard = false, supportGenealogy = true))
+
+
+  "DEGA+ on OneMax" should "work well with naive population w/o genealogy" in
+    simpleTest(64, 96, 128, 150)
+              (n => 2.1 * n * math.log(n))
+              (DEGAPlus.withStandardBitMutation)
+              (n => Problems.naiveOneMaxFT(n, allowDuplicates = true, disableDiscard = false, supportGenealogy = false))
+  
+  it should "work well with naive population with genealogy" in
+    simpleTest(64, 96, 128, 150)
+              (n => 2.1 * n * math.log(n))
+              (DEGAPlus.withStandardBitMutation)
               (n => Problems.naiveOneMaxFT(n, allowDuplicates = true, disableDiscard = false, supportGenealogy = true))
