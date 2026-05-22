353ec4a1fbda5d2eae7364f5e5a66cbe241b675a
Maxim Buzdalov <mbuzdalov@gmail.com>
2026-05-22 14:37:08 +0100

    Factor out common unit-benchmarking code into a mixin.

--- src/test/scala/com/github/mbuzdalov/patchga/IncrementalOneMaxTests.scala
+++ src/test/scala/com/github/mbuzdalov/patchga/IncrementalOneMaxTests.scala
@@ -1,35 +1,11 @@
 package com.github.mbuzdalov.patchga
 
-import com.github.mbuzdalov.patchga.algorithm.{DEGAPlus, MuPlusOneGA, NeverForgettingGA, OnePlusLLGA, OnePlusOneEA, Optimizer}
-import com.github.mbuzdalov.patchga.distribution.{BinomialDistribution, PowerLawDistribution}
-import com.github.mbuzdalov.patchga.infra.FixedTargetTerminator
+import com.github.mbuzdalov.patchga.algorithm.*
+import com.github.mbuzdalov.patchga.distribution.BinomialDistribution
 import com.github.mbuzdalov.patchga.problem.Problems
-import com.github.mbuzdalov.patchga.util.Loops
 import org.scalatest.flatspec.AnyFlatSpec
-import org.scalatest.matchers.should.Matchers
 
-class IncrementalOneMaxTests extends AnyFlatSpec with Matchers:
-  private case class RunResults(avgEvaluations: Double, avgTime: Double)
-
-  private def run(optimizer: Optimizer, target: Int)
-                 (problem: => optimizer.RequiredConfig & Problems.IntProblem): RunResults =
-    val nRuns = 10
-    var sumEvaluations = 0.0
-    val tBegin = System.nanoTime()
-    Loops.repeat(nRuns):
-      val instance = problem
-      sumEvaluations += FixedTargetTerminator.runUntilTargetReached(optimizer, instance, target).nEvaluations
-    RunResults(sumEvaluations / nRuns, (System.nanoTime() - tBegin) * 1e-9 / nRuns)
-
-  private def simpleTest(ns: Int*)
-                        (expected: Int => Double)
-                        (optimizer: Optimizer)
-                        (problem: Int => optimizer.RequiredConfig & Problems.IntProblem): Unit =
-    for n <- ns do
-      val expectedEvs = expected(n)
-      val RunResults(evs, _) = run(optimizer, n)(problem(n))
-      evs shouldBe expectedEvs +- (0.3 * expectedEvs)
-
+class IncrementalOneMaxTests extends IntProblemTestsBase:
   "RLS on OneMax" should "work well with single-slot MST-based population" in
     simpleTest(256, 512, 1024)
               (n => n * math.log(n))
---
+++ src/test/scala/com/github/mbuzdalov/patchga/IntProblemTestsBase.scala
@@ -0,0 +1,30 @@
+package com.github.mbuzdalov.patchga
+
+import com.github.mbuzdalov.patchga.algorithm.Optimizer
+import com.github.mbuzdalov.patchga.infra.FixedTargetTerminator
+import com.github.mbuzdalov.patchga.problem.Problems
+import com.github.mbuzdalov.patchga.util.Loops
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should
+
+trait IntProblemTestsBase extends AnyFlatSpec with should.Matchers:
+  case class RunResults(avgEvaluations: Double, avgTime: Double)
+  
+  def run(optimizer: Optimizer, target: Int)
+                 (problem: => optimizer.RequiredConfig & Problems.IntProblem): RunResults =
+    val nRuns = 10
+    var sumEvaluations = 0.0
+    val tBegin = System.nanoTime()
+    Loops.repeat(nRuns):
+      val instance = problem
+      sumEvaluations += FixedTargetTerminator.runUntilTargetReached(optimizer, instance, target).nEvaluations
+    RunResults(sumEvaluations / nRuns, (System.nanoTime() - tBegin) * 1e-9 / nRuns)
+  
+  def simpleTest(ns: Int*)
+                (expected: Int => Double)
+                (optimizer: Optimizer)
+                (problem: Int => optimizer.RequiredConfig & Problems.IntProblem): Unit =
+    for n <- ns do
+      val expectedEvs = expected(n)
+      val RunResults(evs, _) = run(optimizer, n)(problem(n))
+      evs shouldBe expectedEvs +- (0.3 * expectedEvs)
